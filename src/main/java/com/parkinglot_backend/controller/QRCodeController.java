e753d3e0eaecccfb8b463a81f6ab6bf3628b1b0f
thisXuan <1941427986@qq.com>
2024-12-26 23:00:07 +0800

    修复post方法bug

--- src/main/java/com/parkinglot_backend/controller/QRCodeController.java
+++ src/main/java/com/parkinglot_backend/controller/QRCodeController.java
@@ -6,6 +6,7 @@ import com.parkinglot_backend.util.JwtUtils;
 import com.parkinglot_backend.util.Result;
 import io.jsonwebtoken.Claims;
 //import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,8 +28,8 @@ public class QRCodeController {
     private QRcodeService qrCodeService;
 
     //这里前端使用POST但是后端使用GET,原因不知
-    @GetMapping("/submit")
-    public Result<?> submitQRCode(@RequestBody QRCodeRequest qrCodeRequest, @RequestHeader("Authorization") String token) {
+    @PostMapping("/submit")
+    public Result<?> submitQRCode(@RequestBody QRCodeRequest qrCodeRequest, @RequestHeader("token") String token, HttpServletRequest httpServletRequest) {
         System.out.println("接收到的请求方法: POST");
         System.out.println("接收到的二维码内容: " + qrCodeRequest.getQrCodeContent());
         System.out.println("接收到的 Authorization: " + token);
